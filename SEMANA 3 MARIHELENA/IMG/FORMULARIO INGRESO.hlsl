78acbffeb338a1157e0449b33e4b1ad948358c06
MarihelenaEsquivel <161900184+MarihelenaEsquivel@users.noreply.github.com>
2024-03-01 21:31:06 -0500

    Add files via upload

---
+++ SEMANA 3 MARIHELENA/IMG/FORMULARIO INGRESO.hlsl
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FORMULARIO REGISTRO</title>
+    <!-- SESION DE LINK CSS -->
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <div class="contenedor">
+        <header><img src="./482226505664.png" alt="" width="150" height="150"></header> 
+    <h1> FORMULARIO REGISTRO</h1>
+    </div>
+    <!-- SECCION DE CONTENIDO PRINCIPAL -->
+    <main> 
+        <form action="" class="form_register">
+        <input type="text" placeholder="&#9889 ingrese usuario" required autofocus>
+        <br>
+        
+        <input type="password" placeholder="&#9978 ingrese password" required>
+        <br>
+        
+        <div id="terminos">
+        <input type="checkbox" name=""> Mostrar contraseña
+    <div> 
+        <br>
+        <div class="btn_accion">
+        <button type="submit" class="btn_enviar">ENVIAR</button>
+        <button type="reset" class="btn_limpiar">CANCELAR</button>
+    
+        </form>
+"></form>
+    </main>
+    <footer>
+        &copy;Todos los derechos reservados @MarihelenaEsquivel
+        <footer>
+        <div>
+</body>
+</html>
